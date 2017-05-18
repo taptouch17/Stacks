cc7f63fdf709dda09f555973b6439f74d1960edb
Jelani <jelani@Jelanis-Mac.local>
2017-05-18 11:59:26 -0400

    Initial Commit

---
+++ Stacks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stacks
+//
+//  Created by Jelani on 18/05/2017.
+//  Copyright © 2017 Tap. TOUCH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
